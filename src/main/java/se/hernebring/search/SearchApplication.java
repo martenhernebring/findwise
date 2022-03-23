4c8a02b773523edb94c7be3bebf32c16578444b1
Marten Hernebring <marten.hernebring@gmail.com>
2022-03-23 10:14:38 +0100

    Args must not be null or empty

--- src/main/java/se/hernebring/search/SearchApplication.java
+++ src/main/java/se/hernebring/search/SearchApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SearchApplication {
 
   public static void main(String[] args) {
+    if(args != null && args.length < 1) {
+      throw new IllegalArgumentException("Args must not be empty");
+    }
     SpringApplication.run(SearchApplication.class, args);
   }
 
--- src/test/java/se/hernebring/search/SearchApplicationTests.java
+++ src/test/java/se/hernebring/search/SearchApplicationTests.java
@@ -3,11 +3,25 @@ package se.hernebring.search;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @SpringBootTest
 class SearchApplicationTests {
 
   @Test
-  void contextLoads() {
+  void nullArgumentIsIllegal() {
+    var ex = assertThrows(IllegalArgumentException.class,
+      () -> SearchApplication.main(null));
+    assertEquals("Args must not be null", ex.getMessage());
+  }
+
+  @Test
+  void emptyArgumentIsIllegal() {
+    String[] empty = new String[0];
+    var ex = assertThrows(IllegalArgumentException.class,
+      () -> SearchApplication.main(empty));
+    assertEquals("Args must not be empty", ex.getMessage());
   }
 
 }
